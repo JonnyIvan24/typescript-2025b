0e3039aa5f32ee65022acb1103fe07d16913862a
JonnyIvan24 <johnny.perez.u@gmail.com>
2025-12-13 10:47:40 -0600

    callbacks

--- src/funciones.ts
+++ src/funciones.ts
@@ -18,3 +18,16 @@ const concatWithDefault = (a: string, b: string = "default") => {
 
 console.log(concatValues("hola ", "mundo"))
 console.log(concatWithDefault("hola "))
+
+// callback
+
+const miCallback = function (texto: string) {
+    console.log("impresión desde el callback " + texto)
+}
+
+function funcionConCallback(texto:string, callback: (texto: string) => void) {
+    console.log("impresión dedsde la fucnión " + texto)
+    callback(texto)
+}
+
+funcionConCallback("hola", miCallback)
