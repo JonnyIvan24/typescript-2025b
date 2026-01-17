0ff3e8927dffeec369461690e63df16851da48fd
JonnyIvan24 <johnny.perez.u@gmail.com>
2026-01-17 09:49:42 -0600

    Restricciones de genericos

--- src/Genericos.ts
+++ src/Genericos.ts
@@ -7,3 +7,26 @@ function print<T, I>(a: T, b: I) {
 print<number, string>(1, "")
 print<number, number>(1, 2)
 print<string, number>("", 2)
+
+interface PrintId {
+    id: number;
+    print: () => void;
+}
+
+interface PrintName {
+    name: string;
+    print: () => void;
+}
+
+interface Print extends PrintId, PrintName {
+    description: string;
+}
+
+
+function printUseT<T extends PrintId | PrintName> (item: T) {
+    item.print();
+    item.id
+    item.name
+}
+
+printUseT({print: () =>  console.log("Hola"), name: "hola", id: 1, description: "hola"})
