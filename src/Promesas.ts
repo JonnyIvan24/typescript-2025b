2f0d38a87786f9f4d9b4d2dc340e5c231d15f85b
JonnyIvan24 <johnny.perez.u@gmail.com>
2026-01-17 11:05:57 -0600

    Promesas - async y await

--- src/Promesas.ts
+++ src/Promesas.ts
@@ -4,10 +4,14 @@ function imprimir() {
     console.log("Hola")
 }
 
-fetch("/users").then(
-    (resp) => {
-        console.log(resp.body)
-    }
-).catch(
-    (error) => console.error(error)
-)
+
+async function getItems(url: string) {
+    const response = await fetch(url)
+    const items = await response
+    return items
+}
+
+async function get() {
+    return await Promise.all([getItems("/users"), getItems("/products")])    
+}
+
